e5f911bc75174244584185a1dedbe566fcb0c93a
snidbert64 <zachalotz@gmail.com>
2018-12-14 00:59:56 -0600

    Basic code done. Might switch out a few aesthetic stuff, and rebalance character stats.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,26 +1,29 @@
 
+var humanChar;
+var opponent;
+var defeatedFoes = 0;
 
-
-var characters = [
-    {
+var charList = ["Vader", "Trooper", "Sand", "Luke"];
+var characters = {
+    "Vader" : {
         name : "Darth Vader",
         id: "Vader",
         imageAlive : "assets/images/Darth_Vader.jpg",
-        health : 150,
+        health : 1500,
         baseAttack : 5,
         attack : 5,
         counterAttack : 20
     },
-    {
+    "Trooper" : {
         name : "Stormtrooper",
         id : "Trooper",
         imageAlive : "assets/images/StormTrooperAlive.jpg",
         health : 100,
-        baseAttack : 5,
-        attack : 5,
+        baseAttack : 10,
+        attack : 10,
         counterAttack : 20
     },
-    {
+    "Sand" : {
         name : "Sandtrooper",
         id : "Sand",
         imageAlive : "assets/images/Sandtrooper_DICE.png",
@@ -29,7 +32,7 @@ var characters = [
         attack : 5,
         counterAttack : 20
     },
-    {
+    "Luke" : {
         name : "Luke Skywalker",
         id : "Luke",
         imageAlive : "assets/images/Luke-Skywalker.jpg",
@@ -38,24 +41,63 @@ var characters = [
         attack : 5,
         counterAttack : 20
     }
-]
+}
 
 function mainMenu() {
     $("#play-button").on( "click", function() {
         $("#play-button").hide();
-        for (var i = 0; i < characters.length; i++) {
-            var characterCard = $("<div id='" + characters[i].id + "' class='card col-md-2 character-card'></div>")
-            characterCard.append($("<img height=150 width=150 class='card-img-top' src='" + characters[i].imageAlive + "'>"));
-            characterCard.append($("<h5 class='card-title'>" + characters[i].name + "</h5>"));
-            characterCard.append($("<p class='card-text'>Health: " + characters[i].health + "</p>"));
+        for (var i = 0; i < charList.length; i++) {
+            var characterCard = $("<div id='" + characters[charList[i]].id + "' class='card col-md-2 character-card opponent'></div>")
+            characterCard.append($("<img height=150 width=150 class='card-img-top' src='" + characters[charList[i]].imageAlive + "'>"));
+            characterCard.append($("<h5 class='card-title'>" + characters[charList[i]].name + "</h5>"));
+            characterCard.append($("<p class='card-text'>Health: " + "<span id='" + characters[charList[i]].id + "-health'>" + characters[charList[i]].health + "</span></p>"));
             characterCard.appendTo($("#characters"));
         }
         $("#prompt").text("Choose your character");
         $(".character-card").on( "click", function() {
+            $(".character-card").off("click");
+            humanChar = this.id;
+            $("#" + this.id).removeClass("opponent");
             $("#" + this.id).clone().appendTo($("#fighters"));
             this.remove();
             $("#prompt").text("Click any opponent to fight!");
+            game();
         });
+    });
+}
+
+
+function game() {
+    $("#prompt").text("Click any opponent to fight!");
+    $(".opponent").on("click", function() {
+        $(".opponent").off("click");
+        opponent = this.id;
+        $("#" + this.id).clone().appendTo($("#fighters"));
+        this.remove();
+        $("#prompt").text("Click your chosen opponent to attack");
+        $("#" + opponent).on("click", function() {
+            characters[opponent].health += -characters[humanChar].attack;
+            if (characters[opponent].health <= 0) {
+                $("#" + opponent).remove();
+                defeatedFoes++;
+                if (defeatedFoes === charList.length - 1) {
+                    $("#prompt").html("You Win! Click <a href='index.html'>here</a> to restart.");
+                } else {
+                game();
+                }
+            } else {
+                $("#" + opponent + "-health").text(characters[opponent].health);
+                characters[humanChar].attack += characters[humanChar].baseAttack;
+                characters[humanChar].health += -characters[opponent].counterAttack;
+                if (characters[humanChar].health <= 0) {
+                    $("#" + humanChar + "-health").text(0);
+                    $("#prompt").html("You Lose! Click <a href='index.html'>here</a> to restart.");
+                } else {
+                    $("#" + humanChar + "-health").text(characters[humanChar].health);
+                }
+            }
+        });
+            
     });
 }
 
